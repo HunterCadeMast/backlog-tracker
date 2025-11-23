796ab580c494c430373c5ea7d0a301d2d402f893
Hunter Mast <huntercademast@gmail.com>
2025-11-22 20:45:18 -0500

    Created Django models for user's playlists.
    Integrated Playlists and PlaylistLogs

---
+++ backend/playlists/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from profiles.models import Profiles
+from logs.models import Logs
+import uuid
+
+class Playlists(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False, null = False, blank = False)
+    profile_id = models.ForeignKey(Profiles, on_delete = models.CASCADE, null = False, blank = False)
+    playlist_title = models.CharField(max_length = 32, null = False, blank = False)
+    creation_timestamp = models.DateTimeField(default = timezone.now, null = False, blank = False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields = ['profile_id', 'playlist_title'], name = 'User\'s Playlists')
+        ]
+
+    def __str__(self):
+        return f"{self.profile_id.username} created a playlist called {self.playlist_title}"
+    
+class PlaylistLogs(models.Model):
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False, null = False, blank = False)
+    playlist_id = models.ForeignKey(Playlists, on_delete = models.CASCADE, null = False, blank = False)
+    log_id = models.ForeignKey(Logs, on_delete = models.CASCADE, null = False, blank = False)
+    ordered_playlist_position = models.IntegerField(null = True, blank = True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields = ['playlist_id', 'log_id'], name = 'Entries in Playlist')
+        ]
+
+    def __str__(self):
+        return f"{self.log_id.game_id.game_title} is in the playlist called {self.playlist_id.playlist_title}"
