8160a6a837d1fd520f806ca0e42cf7c7e2f122f3
Hunter Mast <huntercademast@gmail.com>
2025-12-08 17:29:01 -0500

    Changed last_fetched for API key.

--- backend/accounts/permissions.py
+++ backend/accounts/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework_api_key.permissions import HasAPIKey
+from django.utils import timezone
+from profiles.models import APIKeys
 
 class IsAccountOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -7,4 +9,23 @@ class IsAccountOwner(BasePermission):
     
 class APIKeyAuthenticated(BasePermission):
     def has_permission(self, request, view):
-        return (IsAuthenticated().has_permission(request, view) or HasAPIKey().has_permission(request, view))
+        if IsAuthenticated().has_permission(request, view):
+            return True
+        else:
+            api_key_check = HasAPIKey()
+            if not api_key_check.has_permission(request, view):
+                return False
+            raw_api_key = request.headers.get('Authorization')
+            if not raw_api_key:
+                return False
+            split_key = raw_api_key.split()
+            if len(split_key) != 2:
+                return False
+            api_key_prefix = split_key[1][:8]
+            try:
+                api_key = APIKeys.objects.get(api_token = api_key_prefix)
+            except APIKeys.DoesNotExist:
+                return False
+            api_key.last_fetched = timezone.now()
+            api_key.save(update_fields = ['last_fetched'])
+            return True
