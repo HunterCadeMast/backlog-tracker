d33e8c89fd6065d534f72de88660c4acbc4332f0
Hunter Mast <huntercademast@gmail.com>
2025-12-21 18:28:32 -0500

    Fixed issue with alignment of sidebar.

--- frontend/backlog-tracker-next-js/app/components/navigation/sidebar/index.tsx
+++ frontend/backlog-tracker-next-js/app/components/navigation/sidebar/index.tsx
@@ -10,7 +10,7 @@ const Sidebar = ({navigationState, navigationToggle}: SidebarProps) => {
     return (
         <>
             <div className = {`fixed w-full h-full overflow-hidden justify-center grid pt-30 left-0 z-10 bg-cream ${navigationState ? "opacity-100 top-0 pointer-events-auto" : "opacity-0 -top-full pointer-events-none"}`}>
-                <button className = "absolute right-2.5 p-5.5" onClick = {navigationToggle}>
+                <button className = "absolute right-0 p-8" onClick = {navigationToggle}>
                     <div className = "container">
                         <div className = {`w-10 h-1 bg-gray-800 m-1.5 transition-all duration-500 ${navigationState ? `translate-y-2.5 -rotate-45` : ''}`}></div>
                         <div className = {`w-10 h-1 bg-gray-800 m-1.5 transition-all duration-500 ${navigationState ? `opacity-0` : ''}`}></div>
