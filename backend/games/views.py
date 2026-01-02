e52376faa22def162417590ac054c792e4b180f5
Hunter Mast <huntercademast@gmail.com>
2026-01-01 21:35:04 -0500

    Refined search.

--- backend/games/views.py
+++ backend/games/views.py
@@ -1,34 +1,47 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.decorators import action
 from games.models import Games
 from games.serializers import GamesSerializer
 from games.services import request_game_info_by_name
-from games.transfer import games_database_transfer
+import requests
 
 class GamePagination(PageNumberPagination):
     page_size = 50
-    page_size_query_param = 'page_size'
+    page_size_query_param = "page_size"
     max_page_size = 1000
 
-class GamesViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = GamesSerializer
+class GamesViewSet(viewsets.ViewSet):
     authentication_classes = []
     permission_classes = [AllowAny]
     pagination_class = GamePagination
 
-    def get_queryset(self):
+    def list(self, request):
+        search = request.query_params.get("search", "").strip()
         queryset = Games.objects.all()
-        search = self.request.query_params.get('search')
         if not search:
-            return queryset
-        filtered_games = queryset.filter(game_title__icontains = search)
-        if filtered_games.exists() and len(search) >= 3:
-            return filtered_games
-        igdb_data = request_game_info_by_name(search)
-        if not igdb_data:
-            return queryset.none()
-        for game_data in igdb_data:
-            games_database_transfer(game_data)
-        return Games.objects.filter(game_title__icontains = search)
+            paginator = self.pagination_class()
+            page = paginator.paginate_queryset(queryset, request)
+            serializer = GamesSerializer(page, many = True)
+            return paginator.get_paginated_response(serializer.data)
+        try:
+            igdb_titles = request_game_info_by_name(search)
+        except requests.HTTPError:
+            igdb_titles = []
+        local_titles = {game.game_title for game in queryset}
+        total_titles = []
+        for game in igdb_titles:
+            if game["name"] in local_titles:
+                title = queryset.get(game_title = game["name"])
+                total_titles.append(title)
+            else:
+                total_titles.append(Games(id = game.get("id") or 0, game_title = game["name"], cover_artwork_link = game.get("cover", {}).get("url"),))
+        if len(search) >= 3:
+            local_search = queryset.filter(game_title__icontains = search)
+            for game in local_search:
+                if game not in total_titles:
+                    total_titles.append(g)
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(total_titles, request)
+        serializer = GamesSerializer(page, many = True)
+        return paginator.get_paginated_response(serializer.data)
