44ed0d3e4623abad2b6d3cf7b22adffe8de6fc45
Hunter Mast <huntercademast@gmail.com>
2026-01-22 15:25:25 -0500

    Finishing Railway requirements.

--- backend/backlog_tracker_django/settings.py
+++ backend/backlog_tracker_django/settings.py
@@ -71,6 +71,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,6 +145,11 @@ API_KEY = {
 CORS_ALLOWED_ORIGINS = [
     "https://gaminglogjam.com",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://gaminglogjam.com",
+]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -276,9 +282,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
