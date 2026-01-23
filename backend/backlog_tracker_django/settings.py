a4c5442c788ed86e3bb62cfe2dd52b22925c77d8
Hunter Mast <huntercademast@gmail.com>
2026-01-23 14:26:01 -0500

    Profile photos still not updating?

--- backend/backlog_tracker_django/settings.py
+++ backend/backlog_tracker_django/settings.py
@@ -309,31 +309,16 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STORAGES = {
-    'default': {
-        'BACKEND': 'storages.backends.s3.S3Storage',
-    },
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    },
-}
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.getenv('R2_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('R2_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('R2_BUCKET_NAME')
-
-AWS_S3_CUSTOM_DOMAIN = 'media.gaminglogjam.com'
-AWS_S3_ENDPOINT_URL = None
-
-AWS_S3_REGION_NAME = 'auto'
-AWS_S3_ADDRESSING_STYLE = 'virtual'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-AWS_DEFAULT_ACL = None
+AWS_S3_ENDPOINT_URL = f'https://{os.getenv("R2_ACCOUNT_ID")}.r2.cloudflarestorage.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.r2.dev'
 AWS_QUERYSTRING_AUTH = False
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
