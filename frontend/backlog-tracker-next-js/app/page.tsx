4ba9ff17034a7a43d9b743989afae0fc1a9ca6f8
Hunter Mast <huntercademast@gmail.com>
2026-01-29 11:31:08 -0500

    Started changing formatting for mobile.

--- frontend/backlog-tracker-next-js/app/page.tsx
+++ frontend/backlog-tracker-next-js/app/page.tsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <>
       <div className = "base-background overflow-hidden">
-        <h1 className = "font-main-title text-[clamp(4rem,12vw,12rem)] text-white scale-y-180 origin-center pl-12 animate-fade-left"><RandomColor constant>Gaming</RandomColor></h1>
-        <h1 className = "font-main-title text-[clamp(4rem,12vw,12rem)] text-white scale-y-180 origin-center pr-12 self-end animate-fade-right"><RandomColor constant>Logjam</RandomColor></h1>
+        <h1 className = "font-main-title text-[clamp(3rem,10vw,12rem)] text-white scale-y-125 md:scale-y-180 origin-center pl-4 md:pl-12 animate-fade-left motion-safe:animate-fade-left"><RandomColor constant>Gaming</RandomColor></h1>
+        <h1 className = "font-main-title text-[clamp(3rem,10vw,12rem)] text-white scale-y-125 md:scale-y-180 origin-center pr-4 md:pr-12 self-end animate-fade-right motion-safe:animate-fade-right"><RandomColor constant>Logjam</RandomColor></h1>
       </div>
     </>
   );
