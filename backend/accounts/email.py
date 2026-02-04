d49d8a1e7b8d935793dc78f04a9db586b255bb77
Hunter Mast <huntercademast@gmail.com>
2026-02-03 23:40:10 -0500

    Trying to fix email.

--- backend/accounts/email.py
+++ backend/accounts/email.py
@@ -1,14 +1,17 @@
 import requests
 from django.core.mail.backends.base import BaseEmailBackend
+import os
 
 class EmailBackend(BaseEmailBackend):
     def send_messages(self, email_messages):
         if not email_messages:
             return 0
         sent_count = 0
+        api_key = os.getenv('MAILGUN_API_KEY')
+        domain = os.getenv('MAILGUN_DOMAIN', 'mg.gaminglogjam.com')
         for message in email_messages:
             data = {'from': message.from_email, 'to': message.to, 'subject': message.subject, 'text': message.body,}
-            response = requests.post(f'https://api.mailgun.net/v3/{message.extra_headers.get('MAILGUN_DOMAIN') or 'mg.gaminglogjam.com'}/messages', auth = ('api', message.extra_headers.get('MAILGUN_API_KEY') or ''), data = data, timeout = 10,)
+            response = requests.post(f'https://api.mailgun.net/v3/{domain}/messages', auth = ('api', api_key), data = data, timeout = 10,)
             if response.status_code == 200:
                 sent_count += 1
             else:
